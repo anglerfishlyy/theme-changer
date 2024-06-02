64a5516581c98d819b205ac37db0ee7e2af7a51c
anglerfishlyy <kavyasamudralaA7@outlook.com>
2024-06-02 15:52:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './index.jsx';
+import LightDarkMode from './index.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      <LightDarkMode/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import './index.css';
+import useLocalStroage from './useLocalStorage';
+import React from 'react';
+
+export default function LightDarkMode() {
+  const [theme, setTheme] = useLocalStroage("theme", "dark");
+
+  function handleToggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  console.log(theme);
+
+  return (
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <p>Hello World !</p>
+        <button onClick={handleToggleTheme}>Change Theme</button>
+      </div>
+    </div>
+  );
+}
